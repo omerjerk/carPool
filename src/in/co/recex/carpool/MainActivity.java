c41df8f17794a1cca2466786186f2a48d85241c9
omerjerk <omerjerk@omerjerk-ubuntu.(none)>
2013-06-09 10:26:02 +0530

    GCM + Manifest updated

--- src/in/co/recex/carpool/MainActivity.java
+++ src/in/co/recex/carpool/MainActivity.java
@@ -2,14 +2,27 @@ package in.co.recex.carpool;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
+import com.google.android.gcm.GCMRegistrar;
+
 public class MainActivity extends Activity {
+	public static final String SENDER_ID = "323778949148";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		GCMRegistrar.checkDevice(this);
+        GCMRegistrar.checkManifest(this);
+        final String regId = GCMRegistrar.getRegistrationId(this);
+        if (regId.equals("")) {
+          GCMRegistrar.register(this, "796902621769");
+        } else {
+          Log.v("message", "Already registered"); 
+        }
 	}
 
 	@Override
